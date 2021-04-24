6df9a93b4478f8db5ab2db237357de5c147369e9
Abanoub-Asaad <Abanoub.Asaad200010@gmail.com>
2021-04-24 16:26:01 +0200

    Add google maps activity

--- MTMsTask/app/src/main/java/com/example/mtms_task/MapActivity.java
+++ MTMsTask/app/src/main/java/com/example/mtms_task/MapActivity.java
@@ -16,8 +16,5 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
 }
